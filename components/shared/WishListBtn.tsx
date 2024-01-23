79ae2fe96cc9db2dabdb1a2cf1d2f4548f753691
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-01-23 19:26:38 +0530

    Implement tooltip ui

--- components/shared/WishListBtn.tsx
+++ components/shared/WishListBtn.tsx
@@ -2,6 +2,12 @@
 
 import Image from "next/image";
 import { Button } from "../ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const addToWishList = (productId: string) => {
   console.log("Product added to WishList");
@@ -9,12 +15,26 @@ const addToWishList = (productId: string) => {
 
 const WishListBtn = ({ productId }: { productId: string }) => {
   return (
-    <Button
-      className="absolute top-0 right-0 bg-slate-50/40 hover:bg-slate-100/50 w-fit h-fit"
-      onClick={() => addToWishList(productId)}
-    >
-      <Image src="/icons/wishlist.svg" alt="wishlist" width={15} height={18} />
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            className="absolute top-0 right-0 bg-slate-50/40 hover:bg-slate-100/50 w-fit h-fit"
+            onClick={() => addToWishList(productId)}
+          >
+            <Image
+              src="/icons/wishlist.svg"
+              alt="wishlist"
+              width={15}
+              height={18}
+            />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Add to Wishlist</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 export default WishListBtn;
