38e98b072194443087cd1c8b6b2d848d0958fc3c
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-01-26 18:10:27 +0530

    Implement Basic empty cart screen

--- components/shared/CartItems.tsx
+++ components/shared/CartItems.tsx
@@ -2,15 +2,28 @@
 
 import { useStore } from "@/lib/store";
 import CartItem from "./CartItem";
+import Link from "next/link";
 
 const CartItems = () => {
   const { cart } = useStore();
-  return (
-    <div className="flex flex-1 flex-col gap-y-2">
-      {cart.map((item) => (
-        <CartItem key={item.productId} cartItem={item} />
-      ))}
-    </div>
-  );
+
+  if (cart.length > 0) {
+    return (
+      <div className="flex flex-1 flex-col gap-y-2">
+        {cart.map((item) => (
+          <CartItem key={item.productId} cartItem={item} />
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-full h-[89vh] flex items-center justify-center">
+        Cart is empty.{" "}
+        <Link href="/" className="hover:underline text-lg font-medium">
+          Continue Shopping...
+        </Link>
+      </div>
+    );
+  }
 };
 export default CartItems;
--- components/shared/CartSummary.tsx
+++ components/shared/CartSummary.tsx
@@ -10,18 +10,23 @@ const CartSummary = () => {
   useEffect(() => {
     calculateTotalPrice();
   }, [cart]);
-  return (
-    <div className="w-full sm:w-[40%] flex items-start justify-center mx-auto">
-      <div className="border-2 w-full max-lg:my-4 sm:w-[600px] lg:w-[300px] py-2 px-4 border-slate-400 ">
-        <h3 className="text-lg font-medium">Order Summary</h3>
-        <CartSummaryRow text="Subtotal" price={totalPrice} />
-        <CartSummaryRow text="Discount" price={0} />
-        <CartSummaryRow text="Total" price={totalPrice} />
-        <Button className="w-full mt-4 mb-2" variant="outline">
-          Checkout
-        </Button>
+
+  if (cart.length > 0) {
+    return (
+      <div className="w-full sm:w-[40%] flex items-start justify-center mx-auto">
+        <div className="border-2 w-full max-lg:my-4 sm:w-[600px] lg:w-[300px] py-2 px-4 border-slate-400 ">
+          <h3 className="text-lg font-medium">Order Summary</h3>
+          <CartSummaryRow text="Subtotal" price={totalPrice} />
+          <CartSummaryRow text="Discount" price={0} />
+          <CartSummaryRow text="Total" price={totalPrice} />
+          <Button className="w-full mt-4 mb-2" variant="outline">
+            Checkout
+          </Button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 };
 export default CartSummary;
