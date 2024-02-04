60b99808e6a0bb0a8ac81df194d560cbd436083a
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-02-04 09:14:01 +0530

    Implement Order model

---
+++ lib/mongodb/models/order.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model, models } from "mongoose";
+
+const OrderSchema = new Schema({
+  buyerId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  products: [
+    {
+      productId: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+      qty: {
+        type: Number,
+        required: true,
+        default: 1,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  stripeId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  totalAmount: {
+    type: String,
+  },
+});
+
+const Order = models.Order || model("Order", OrderSchema);
+
+export default Order;
