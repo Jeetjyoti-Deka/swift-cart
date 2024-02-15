8469226d17c9764a9e20e5bac9a33fc484776aa8
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-02-15 22:15:05 +0530

    Fix mobile nav img issue

--- components/shared/MobileNavLink.tsx
+++ components/shared/MobileNavLink.tsx
@@ -24,7 +24,7 @@ const MobileNavLink = ({
         } w-full flex items-center justify-start gap-x-2 rounded-[8px] px-4 py-2 `}
       >
         <Image
-          src={`/icons/${label}.svg`}
+          src={`/icons/${label.toLowerCase()}.svg`}
           alt={`${label}`}
           width={24}
           height={24}
